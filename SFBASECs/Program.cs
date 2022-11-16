6d748828826d27d90dd0f40575e1858cfc1e7368
alimskeetit <101967111+alimskeetit@users.noreply.github.com>
2022-11-16 12:32:17 +0300

    asd

---
+++ SFBASECs/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int iterator = 0;
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+        var filter = array.Where(n => n > 3);
+        Print(filter);
+        var newFilter = filter.Select(_ => iterator++).ToArray();
+        Print(newFilter);
+        Console.WriteLine(newFilter.First());
+    }
+
+    static void Print(IEnumerable<int> arg)
+    {
+        foreach (int i in arg) Console.Write(i + " ");
+        Console.WriteLine();
+    }
+}
